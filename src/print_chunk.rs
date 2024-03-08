ef07c775a56b4d8719d138c7ea12429c01b93045
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-03-07 19:39:00 -0800

    remove comfy table

--- src/print_chunk.rs
+++ src/print_chunk.rs
@@ -1,74 +1,82 @@
-use crate::parse_chunk::{
-    get_compression_code_str, DataChunk, FmtChunk, ListInfoChunk, RiffChunk
-};
-use comfy_table::{modifiers::UTF8_ROUND_CORNERS, Row, Table};
+use crate::parse_chunk::{get_compression_code_str, DataChunk, FmtChunk, ListInfoChunk, RiffChunk};
+use owo_colors::OwoColorize;
+
+fn print_rows(rows: Vec<(&str, &str)>) {
+    let max_width = rows.iter().map(|(k, _v)| k.len()).max().unwrap();
+    let line = "-".repeat(max_width);
+
+    println!("{}", format!("╭--{}", &line).dimmed());
+    let table = rows
+        .iter()
+        .map(|(k, v)| {
+            format!(
+                "{} {k}{}{v}",
+                "|".dimmed(),
+                " ".repeat(max_width - k.len() + 4)
+            )
+        })
+        .collect::<Vec<String>>()
+        .join(&format!("\n|---{}\n", &line).dimmed().to_string());
+
+    println!("{}", table);
+    println!("{}", format!("╰--{}", &line).dimmed());
+}
 
 pub fn print_riff_chunk(riff_chunk: &RiffChunk) {
-    let mut table = Table::new();
-    table.apply_modifier(UTF8_ROUND_CORNERS);
-
-    table.add_rows(vec![
-        Row::from(vec!["chunk id", "'RIFF'"]),
-        Row::from(vec![
+    print_rows(vec![
+        ("chunk id", &"'RIFF'".blue().to_string()),
+        (
             "size of file (in bytes)",
-            &riff_chunk.file_size.to_string(),
-        ]),
-        Row::from(vec!["wave identifier", &riff_chunk.wave_ident]),
+            &riff_chunk.file_size.to_string().green().to_string(),
+        ),
+        ("wave identifier", &riff_chunk.wave_ident),
     ]);
-
-    println!("{table}");
 }
 
 pub fn print_fmt_chunk(fmt_chunk: &FmtChunk) {
-    let mut table = Table::new();
-    table.apply_modifier(UTF8_ROUND_CORNERS);
-
-    table.add_rows(vec![
-        Row::from(vec!["chunk id", "'fmt '"]),
-        Row::from(vec![
+    print_rows(vec![
+        ("chunk id", &"'fmt '".blue().to_string()),
+        (
             "size of fmt chunk (in bytes)",
-            &fmt_chunk.chunk_size.to_string(),
-        ]),
-        Row::from(vec![
+            &fmt_chunk.chunk_size.to_string().green().to_string(),
+        ),
+        (
             "compression code",
             &format!(
                 "{} ({})",
                 fmt_chunk.compression_code,
                 get_compression_code_str(fmt_chunk.compression_code),
             ),
-        ]),
-        Row::from(vec![
+        ),
+        (
             "number of channels",
             &fmt_chunk.number_of_channels.to_string(),
-        ]),
-        Row::from(vec!["sampling rate", &fmt_chunk.sample_rate.to_string()]),
-        Row::from(vec!["byte rate", &fmt_chunk.byte_rate.to_string()]),
-        Row::from(vec!["block align", &fmt_chunk.block_align.to_string()]),
-        Row::from(vec![
-            "bits per sample",
-            &fmt_chunk.bits_per_sample.to_string(),
-        ]),
+        ),
+        ("sampling rate", &fmt_chunk.sample_rate.to_string()),
+        ("byte rate", &fmt_chunk.byte_rate.to_string()),
+        ("block align", &fmt_chunk.block_align.to_string()),
+        ("bits per sample", &fmt_chunk.bits_per_sample.to_string()),
     ]);
 
     if let Some(extended_chunk) = &fmt_chunk.extended_fmt_sub_chunk {
-        table.add_rows(vec![
-            Row::from(vec![
+        print_rows(vec![
+            (
                 "Number of valid bits",
                 &extended_chunk.num_valid_bits.to_string(),
-            ]),
-            Row::from(vec![
+            ),
+            (
                 "Speaker position mask",
                 &extended_chunk.channel_mask.to_string(),
-            ]),
-            Row::from(vec![
+            ),
+            (
                 "Actual compression code",
                 &format!(
                     "{} ({})",
                     extended_chunk.compression_code,
                     get_compression_code_str(extended_chunk.compression_code),
                 ),
-            ]),
-            Row::from(vec![
+            ),
+            (
                 "WAV GUID",
                 &extended_chunk
                     .wav_guid
@@ -76,48 +84,46 @@ pub fn print_fmt_chunk(fmt_chunk: &FmtChunk) {
                     .map(|b| format!("{:02X}", b))
                     .collect::<Vec<String>>()
                     .join(" "),
-            ]),
+            ),
         ]);
     }
-
-    println!("{table}");
 }
 
 pub fn print_list_chunk(list_chunk: &ListInfoChunk) {
-    let mut table = Table::new();
-    table.apply_modifier(UTF8_ROUND_CORNERS);
+    let mut table = vec![
+        ("chunk id".to_string(), "'LIST'".blue().to_string()),
+        (
+            "size of LIST chunk (in bytes)".to_string(),
+            list_chunk.chunk_size.to_string().green().to_string(),
+        ),
+    ];
 
-    table.add_rows(vec![
-        Row::from(vec!["chunk id", "'LIST'"]),
-        Row::from(vec![
-            "size of LIST chunk (in bytes)",
-            &list_chunk.chunk_size.to_string(),
-        ]),
-    ]);
-    table.add_rows(
-        list_chunk.data
+    let mut text_chunks: Vec<(String, String)> = list_chunk
+        .data
+        .iter()
+        .map(|sub_chunk| (sub_chunk.info_id.clone(), sub_chunk.text.clone()))
+        .collect();
+
+    table.append(&mut text_chunks);
+
+    print_rows(
+        table
             .iter()
-            .map(|sub_chunk| Row::from(vec![sub_chunk.info_id.clone(), sub_chunk.text.clone()])),
+            .map(|(k, v)| (k.as_str(), v.as_str()))
+            .collect(),
     );
-
-    println!("{table}");
 }
 
 pub fn print_data_chunk(data_chunk: &DataChunk) {
-    let mut table = Table::new();
-    table.apply_modifier(UTF8_ROUND_CORNERS);
-
-    table.add_rows(vec![
-        Row::from(vec!["chunk id", "'data'"]),
-        Row::from(vec![
+    print_rows(vec![
+        ("chunk id", &"'data'".blue().to_string()),
+        (
             "size of data chunk (in bytes)",
-            &data_chunk.chunk_size.to_string(),
-        ]),
-        Row::from(vec![
+            &data_chunk.chunk_size.to_string().green().to_string(),
+        ),
+        (
             "data... (minimized)",
             &format!("[ ...{} items ]", &data_chunk.sample_data.len()),
-        ]),
+        ),
     ]);
-
-    println!("{table}");
 }
